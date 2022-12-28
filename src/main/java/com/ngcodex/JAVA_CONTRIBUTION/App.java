d9d796789f237e2b2a379dd43fde53bde2143dc1
NGUENAZEBS <ngcodex@gmail.com>
2022-12-28 11:50:21 +0100

    maven.apache.org/repositories/artifacts.html

---
+++ src/main/java/com/ngcodex/JAVA_CONTRIBUTION/App.java
@@ -0,0 +1,13 @@
+package com.ngcodex.JAVA_CONTRIBUTION;
+
+/**
+ * Hello world NGcodeX!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World NGcodeX!" );
+    }
+}
---
+++ src/test/java/com/ngcodex/JAVA_CONTRIBUTION/AppTest.java
@@ -0,0 +1,20 @@
+package com.ngcodex.JAVA_CONTRIBUTION;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
